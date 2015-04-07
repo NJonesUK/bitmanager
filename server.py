fff7f6659ff052495bc4a46e9f7d09fd507522a3
Nick Jones <nick@nojones.net>
2015-04-07 01:26:40 +0100

    Added terminate functionality

--- server.py
+++ server.py
@@ -19,18 +19,24 @@ def create_instance():
     url_redirect = "/instance_status?access=" + request.args.get('access') + "&secret=" + request.args.get('secret') + "&id=" + reservation.instances[0].id
     return redirect(url_redirect, code=302)
 
+@app.route('/terminate_instance')
+def create_instance():
+    conn = boto.ec2.connect_to_region("us-west-2", aws_access_key_id=request.args.get('access'), aws_secret_access_key=request.args.get('secret'))
+    conn.terminate_instances(instance_ids=[request.args.get('id')])
+    return render_template("status.html", status=status_code, link=link)
+
 @app.route('/instance_status')
 def instance_status():
     conn = boto.ec2.connect_to_region("us-west-2", aws_access_key_id=request.args.get('access'), aws_secret_access_key=request.args.get('secret'))
     status = conn.get_all_instance_status(instance_ids=[request.args.get('id')])
     try:
-        string = '<html><head><meta http-equiv="refresh" content="5" ></head><body> Current status: ' + status[0].state_name + '</body></html>'
-        return str(status[0].state_name)
-    except IndexError:
-        return '<html><head><meta http-equiv="refresh" content="5" ></head><body> Instance spinning up </body></html>'
-    # if status:
-    #     if status == "Running":
-    #         pass    
+        status_code = status[0].state_code
+    except:
+        status_code = -1
+    link = conn.get_all_instances(instance_ids=[request.args.get('id')])[0].instances[0].public_dns_name
+    terminate_link =  "/terminate_instance?access=" + request.args.get('access') + "&secret=" + request.args.get('secret') + "&id=" + reservation.instances[0].id
+    return render_template("status.html", status=status_code, link=link, terminate_link=terminate_link)
+
 
 if __name__ == '__main__':
     app.debug = True
